ad1a3ed66d93073de1d94fbf0c58b54a0f8fe7f7
Ryan McWilliam <ryanmcwilliam@gmail.com>
2019-10-20 16:28:48 -0400

    fix white space

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,8 +10,8 @@ export default () => (
             <Link path="/about/" text="About" />
         </p>
         <p style={{ fontSize: 18, color: `black` }}>
-            Please reach out if you wou ld like to buy some honey or just want
-            to open up a box of bees.
+            Please reach out if you would like to buy some honey or just want to
+            open up a box of bees.
         </p>
     </div>
 );
