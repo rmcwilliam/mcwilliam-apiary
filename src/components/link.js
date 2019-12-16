b2b49eb842f51029f1c35c302a33195564390f12
Ryan McWilliam <ryanmcwilliam@gmail.com>
2019-12-15 22:48:23 -0500

    add import

--- src/components/link.js
+++ src/components/link.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "gatsby";
+import styled from "styled-components";
 
 const StyledLink = styled(Link)`
     text-decoration: none;
+    color: black;
 `;
 
 export default props => <StyledLink to={props.path}>{props.text}</StyledLink>;
